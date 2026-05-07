8ce63886d8163a440c34aea76d3910436f390c65
roboco <roboco@zoho.eu>
2026-05-07 23:48:40 +0400

    Add env/setup.sh for environment

---
+++ env/setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+AXMOL_VERSION="2.11.3"
+AXMOL_URL="https://github.com/axmolengine/axmol/archive/refs/tags/v${AXMOL_VERSION}.tar.gz"
+AXMOL_DIR="axmol-${AXMOL_VERSION}"
+
+echo "Downloading Axmol ${AXMOL_VERSION}..."
+wget -q --show-progress "$AXMOL_URL" -O axmol.tar.gz
+
+echo "Extracting..."
+tar -xzf axmol.tar.gz
+mv "$AXMOL_DIR" axmol
+rm axmol.tar.gz
+
+echo "Axmol ready at $(pwd)/axmol"
